54770ab9ab652e5dcd602166701e08876335aab1
Himalayan <hdnpress@gmail.com>
2022-04-08 15:44:39 +0545

    List Products with Dummy Data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,21 +6,21 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-   public function index()
-   {
-      $viewData = [];
-      $viewData["title"] = "Home Page - Online Store";
-      return view('home.index')->with("viewData", $viewData);
-   }
+    public function index()
+    {
+        $viewData = [];
+        $viewData["title"] = "Home Page - Online Store";
+        return view('home.index')->with("viewData", $viewData);
+    }
 
-   public function about()
-   {
-      $viewData = [];
-      $viewData["title"] = "Home Page - Online Store";
-      $viewData["subtitle"] = "About us";
-      $viewData["description"] = "This is an about page...";
-      $viewData["author"] = "Developed by: Rajesh Budhathoki";
+    public function about()
+    {
+        $viewData = [];
+        $viewData["title"] = "Home Page - Online Store";
+        $viewData["subtitle"] = "About us";
+        $viewData["description"] = "This is an about page...";
+        $viewData["author"] = "Developed by: Rajesh Budhathoki";
 
-      return view('home.about')->with("viewData", $viewData);
-   }
+        return view('home.about')->with("viewData", $viewData);
+    }
 }

e5ad5a9d8c2346548224a0c6154717c19688955d
Himalayan <hdnpress@gmail.com>
2022-04-10 17:18:57 +0545

    Refactoring List Products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -24,8 +24,8 @@ class ProductController extends Controller
 
         $product = Product::findOrFail($id);
 
-        $viewData["title"] = $product["name"] . "- Online Store";
-        $viewData["subtitle"] = $product["name"] . "- Product Information";
+        $viewData["title"] = $product->getName() . "- Online Store";
+        $viewData["subtitle"] = $product->getName() . "- Product Information";
         $viewData["product"] = $product;
         return view('product.show')->with("viewData", $viewData);
     }

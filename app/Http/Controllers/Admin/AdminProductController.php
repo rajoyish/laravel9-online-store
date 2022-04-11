d954861340edf40b26b8ebdd9ecd1d9f3bd747e3
Himalayan <hdnpress@gmail.com>
2022-04-11 17:53:42 +0545

    List Products in Admin Panel

---
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class AdminProductController extends Controller
+{
+    public function index()
+    {
+        $viewData = [];
+        $viewData["title"] = "Admin Page - Online Store";
+
+        $viewData["products"] = Product::all();
+
+        return view('admin.product.index')->with("viewData", $viewData);
+    }
+}
